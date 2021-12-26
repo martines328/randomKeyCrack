a5eb9c946a9c5fd697a9d59678933e9c68c65f2c
rest2 <rest2928@gmail.com>
2021-12-26 14:38:03 +0200

    init

--- main.py
+++ main.py
@@ -12,21 +12,18 @@ def run():
     while True:
         valid_private_key = False
         while not valid_private_key:
-            #private_key = bitcoin.random_key()
-           # decoded_private_key = bitcoin.decode_privkey(private_key, 'hex')
-            #valid_private_key = 0 < decoded_private_key < bitcoin.N
-            decoded_private_key1 = "0000000000000000000000000000000000000000000000007cce5efdaccf6808"
+            private_key = bitcoin.random_key()
+            decoded_private_key = bitcoin.decode_privkey(private_key, 'hex')
+            valid_private_key = 0 < decoded_private_key < bitcoin.N
 
-        wif_encoded_private_key = bitcoin.encode_privkey(decoded_private_key1, 'wif')
-        public_key = bitcoin.fast_multiply(bitcoin.G, decoded_private_key1)
-        print(wif_encoded_private_key)
-        print(public_key)
-
+        wif_encoded_private_key = bitcoin.encode_privkey(decoded_private_key, 'wif')
+        public_key = bitcoin.fast_multiply(bitcoin.G, decoded_private_key)
 
         r = requests.get("https://blockchain.info/q/getsentbyaddress/"+bitcoin.pubkey_to_address(public_key))
 
         if int(r.text) > 0:
             resultFile = open("result.txt", "w+")
+
             print("Bitcoin Address is:", str(bitcoin.pubkey_to_address(public_key)))
             print("Private Key is: ", str(wif_encoded_private_key))
             print("Balance is: ", str(r.text))
